1c09ca85aad8dad34d3cb2be032f1c5de8c27592
raisul <raisulmondolce@gmail.com>
2025-07-31 18:41:42 +0600

    image name

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -22,7 +22,8 @@ const Weather = () => {
   console.log(data);
 
   return (
-    <div className="min-h-screen bg-[url('images/pi4.jpg')] bg-cover bg-no-repeat">
+    <div className="min-h-screen bg-[url('/images/pic3.jpg')] bg-cover bg-no-repeat">
+
     <div >
     <WeatherInput Searchcity={HandleData }  />
        {error && (
